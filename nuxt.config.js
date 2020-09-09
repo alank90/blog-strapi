fcb7cb7c203a989e08b1f63320f5a091c670a4c5
Alan Killian <akillian@outlook.com>
2020-09-09 18:20:54 -0400

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,4 +84,10 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+  vue: {
+    config: {
+      productionTip: false,
+      devtools: true,
+    },
+  },
 };
